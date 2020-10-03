340fa590947ee1669c26cd14bb9ec8367e0e380c
Crystal Quick <cacquick@gmail.com>
2020-10-03 12:05:06 -0500

    image displayed for toast

--- app/src/main/java/com/example/quickcustomtoastdemo/MainActivity.java
+++ app/src/main/java/com/example/quickcustomtoastdemo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.quickcustomtoastdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,13 +20,25 @@ public class MainActivity extends AppCompatActivity {
 
     // Event to display colored toast
     public void displayColoredToast(View view){
-        Toast toast = Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG);
+        LayoutInflater inflater = getLayoutInflater();
+        View custom_layout = inflater.inflate(R.layout.custom_toast_color, (ViewGroup) findViewById(R.id.custom_toast_color));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(custom_layout);
         toast.show();
     }
 
-    // Event to display colored toast
+    // Event to display an image of toast
     public void displayImageToast(View view){
-        Toast toast = Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG);
+        LayoutInflater inflater = getLayoutInflater();
+        View custom_layout = inflater.inflate(R.layout.custom_toast_image, (ViewGroup) findViewById(R.id.custom_toast_image));
+
+        Toast toast = new Toast(getApplicationContext());
+        // Set Gravity allows you to position your toast
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(custom_layout);
         toast.show();
     }
 
